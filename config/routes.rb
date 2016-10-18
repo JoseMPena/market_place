8bfd0bccf1d2abfd3cf7bf591ec797012890c1db
LegaTux <josmpena91@gmail.com>
2016-10-18 19:38:30 +0200

    Rename branch to setting-version

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api,
   defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do
         
-      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
