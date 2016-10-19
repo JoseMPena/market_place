19e69bdacc3a57de8350b306094894e177a866c1
LegaTux <josmpena91@gmail.com>
2016-10-19 21:15:49 +0200

    Add user endpoint test

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do
-
+        resources :users, only: [:show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Include Devise Controller helper for the controller tests (specs)
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
